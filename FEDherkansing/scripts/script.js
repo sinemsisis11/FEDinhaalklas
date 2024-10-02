6b197fffc100da4b7015288d9e64871dc11a424a
sinemsisis11 <sinem29u@gmail.com>
2024-10-02 11:44:56 +0200

    .

--- FEDherkansing/scripts/script.js
+++ FEDherkansing/scripts/script.js
@@ -1,2 +1,13 @@
-// JavaScript Document
+
 console.log("hi");
+
+const menuOpenButton = document.querySelector("header > button");
+const deNav = document.querySelector("header nav")
+
+
+menuOpenButton.onclick = openMenu;
+
+
+function openMenu() {
+  deNav.classList.add ("toonMenu");
+}
